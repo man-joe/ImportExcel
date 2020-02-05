23e494e9e0bd0a5b41f6f3de6a5927aa75858201
joseph.hoang <joseph.hoang@noaa.gov>
2020-02-05 14:06:32 -0500

    First shot at downloading excel file with razor pages

---
+++ ImportExcelDemo/Data/DemoContext.cs
@@ -0,0 +1,19 @@
+﻿
+using ImportExcelDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExcelDemo.Data
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cmdb> Cmdbs { get; set; }
+    }
+}
---
+++ ImportExcelDemo/Models/Cmdb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExcelDemo.Models
+{
+    public class Cmdb
+    {
+        public int CmdbID { get; set; }
+        [StringLength(50)]
+        public string CDTag { get; set; }
+        [StringLength(50)]
+        public string AdUser{ get; set; }
+        [StringLength(50)]
+        public string Location { get; set; }
+
+    }
+}
