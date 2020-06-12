09999f94b7dd83e78052b53b2a7d4bb12fd9ff57
j-hoang3 <joseph.hoang@noaa.gov>
2020-06-12 12:00:15 -0400

    Added RepoTables Model again...

---
+++ ImportExcelDemo/Models/RepoTables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExcelDemo.Models
+{
+    public class RepoTables
+    {
+        #region Constructor
+        public RepoTables()
+        {
+        }
+        #endregion
+
+        #region Instance Variables
+        [Key]
+        public string UniqueIdentifier { get; set; }
+        
+        public Cmdb Cmdb { get; set; }
+        public Sunflower Sunflower { get; set; }
+        public AD_Computer Ad { get; set; }
+        public EPO Epo { get; set; }
+       /* public ECMO Ecmo { get; set; }
+        public SCCM Sccm { get; set; }*/
+
+        #endregion
+    }
+}
